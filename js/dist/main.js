a7efa22d40629b60e7934cdaae5833846a760643
Taghinia <keivan.taghinia@gmail.com>
2021-08-04 16:11:56 +0100

    Initial try at sticky header

--- js/dist/main.js
+++ js/dist/main.js
@@ -1,1 +1,16 @@
 "use strict";
+
+// Sticky header
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+
+  if (prevScrollpos > currentScrollPos) {
+    $("#sticky").slideDown(150);
+  } else {
+    $("#sticky").slideUp(150);
+  }
+
+  prevScrollpos = currentScrollPos;
+};
---
+++ js/src/main.js
@@ -0,0 +1,12 @@
+// Sticky header
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    $("#sticky").slideDown(150);
+  } else {
+    $("#sticky").slideUp(150);
+  }
+  prevScrollpos = currentScrollPos;
+}
