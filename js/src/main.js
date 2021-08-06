f6a797e7d6c5746d1ec8b7d83608c9d1da5bdc48
Taghinia <keivan.taghinia@gmail.com>
2021-08-06 11:37:06 +0100

    added cookie function

--- js/src/main.js
+++ js/src/main.js
@@ -28,3 +28,48 @@ window.onscroll = function() {
     autoplay: true,
     autoplayHoverPause: true
   });
+
+
+  //cookie popup 
+
+  // cookie popup
+
+var popup = document.getElementById('cookie-container');
+var cookieSettings = document.getElementById('cookie-settings');
+var cookieAccept = document.getElementById('cookie-accept');
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+if (getCookie("accepted") === "" || getCookie("accepted") === null) {
+
+  popup.classList.add('shown');
+  document.body.classList.add('fixed-position');
+
+  cookieAccept.addEventListener('click', function(e) {
+    setCookie("accepted","true",30);
+    popup.classList.remove('shown');
+    console.log("worked");
+  });
+
+}
