acc49b0ed6e5c1840a4620e3d1fb395b241f2289
Taghinia <keivan.taghinia@gmail.com>
2021-10-26 13:08:57 +0100

    update

---
+++ contact.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en" id="top">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Netmatters | Full Service Digital Agency | Norwich, Norfolk | Netmatters</title>
+  <link rel="shorcut icon" href="img/favicon.ico">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+    integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
+    integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="scss/normalize.css">
+  <link rel="stylesheet" href="scss/hamburgers.css">
+  <link rel="stylesheet" href="./css/style.css">
+
+  <!-- scripts -->
+  <script src="https://kit.fontawesome.com/d9dd6a6053.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+  <!-- cookie popup -->
+  <div id="cookie-container">
+    <div class="cookie-inner">
+      <div class="popup-header">
+        <h3>Cookies Policy</h3>
+      </div>
+      <div class="popup-body">
+        <p>
+          We use cookies to obtain aggregate data regarding site traffic and interaction,
+          in order to identify user trends and obtain insights in order to continually
+          improve our site. These cookies enable us to improve your customer experience
+          as you use our site and help provide you with relevant online marketing.
+        </p>
+        <p>
+          You can see a list of the other companies who use cookies on this website, by
+          visiting cookie settings at the bottom of each page. For full details of how
+          we use your personal information, and your rights in relation to it, view
+          our privacy policy.
+        </p>
+      </div>
+      <div class="popup-buttons">
+        <button id="cookie-settings" class="btn button-cookie-settings">Change Settings</button>
+        <button id="cookie-accept" class="btn button-cookie-accept">Accept Cookies</button>
+      </div>
+    </div>
+  </div>
+
+  <!-- header with icon, buttons and search bar. Sticky feature when up scrolling up-->
+  <div id="main-body">
+    <?php include 'inc/header.php'; ?>
+
+
+    <?php include 'inc/footer.php'; ?>
+    </body>
+
+</html>
